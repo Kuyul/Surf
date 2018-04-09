a21ad4f3db71a928e8457a11831e2ff7761af8d4
kyun1991 <38071517+kyun1991@users.noreply.github.com>
2018-04-09 15:32:29 +1200

    Menu
    Menu

---
+++ Surfman/Assets/PauseResumeRestart.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseResumeRestart : MonoBehaviour {
+
+    public bool isPaused = false;
+    public GameObject pausePanel;
+
+    public void PauseGame()
+    {
+        isPaused = true;
+        if (isPaused == true)
+        {
+            Time.timeScale = 0;
+            //GameControl.instance.backGroundMusic.Pause();
+            pausePanel.SetActive(true);
+        }
+
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        if(isPaused==false)
+        {
+            Time.timeScale = 1;
+            pausePanel.SetActive(false);
+        }
+    }
+
+    public void RestartGame(int level)
+    {
+        SceneManager.LoadScene(level);
+        isPaused = false;
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+    }
+
+
+}
