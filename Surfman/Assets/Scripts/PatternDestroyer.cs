3e40967ef7c7aef08b9e07fd91d6afe06ac512ae
Kuyul <peyopeyo92@gmail.com>
2018-04-24 16:54:45 +1200

    BugFixes and enhancements
    Lowered the wave surface level to fix an issue where sometimes double jump is disabled.
    
    Enhancements - We realised that there was a siginificant drop in FPS because up to three patterns could exist, and that means too many instantiated objects.
    
    To fix this:
    Included a second destroyer - which only destroys Obstacles, Coins and Waves.
    
    Pushed window towards the end of the pattern so that new pattern generation is delayed.

---
+++ Surfman/Assets/Scripts/PatternDestroyer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatternDestroyer : MonoBehaviour
+{
+
+    // Use this for initialization
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+
+}
