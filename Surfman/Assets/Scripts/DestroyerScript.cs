1297865a5b3e2b6a92a323e554f20c47292a80b8
Kuyul <peyopeyo92@gmail.com>
2018-04-24 22:02:12 +1200

    Commit Revert

--- Surfman/Assets/Scripts/DestroyerScript.cs
+++ Surfman/Assets/Scripts/DestroyerScript.cs
@@ -6,14 +6,14 @@ public class DestroyerScript : MonoBehaviour
 {
 
     // Use this for initialization
-    void OnTriggerExit2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Hero")
         {
             Debug.Break();
         }
 
-        if (other.tag == "Obstacle" || other.tag == "Coin" || other.tag == "Wave")
+        if (other.tag == "Pattern")
         {
             Destroy(other.gameObject);
         }
