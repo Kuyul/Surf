02c37ea339bd09cc71d910017bf692ed1b8f9950
Kuyul <peyopeyo92@gmail.com>
2018-04-24 22:14:43 +1200

    I don't know what I'm doing

--- Surfman/Assets/Scripts/DestroyerScript.cs
+++ Surfman/Assets/Scripts/DestroyerScript.cs
@@ -6,14 +6,14 @@ public class DestroyerScript : MonoBehaviour
 {
 
     // Use this for initialization
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerExit2D(Collider2D other)
     {
         if (other.tag == "Hero")
         {
             Debug.Break();
         }
 
-        if (other.tag == "Pattern")
+        if (other.tag == "Obstacle" || other.tag == "Coin" || other.tag == "Wave")
         {
             Destroy(other.gameObject);
         }
