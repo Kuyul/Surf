a12e125f493c5230b8e60f38a6e791525ef11ec2
Kuyul <peyopeyo92@gmail.com>
2018-05-01 16:13:49 +1200

    Error message fix
    Error message fix

---
+++ Surfman/Assets/PlayerControl.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public LayerMask whatIsSea;
+    public float initialPlayerSpeed;
+    public float jumpVel = 5.0f;
+    public float balloonVel = 20.0f;
+    private Rigidbody2D rb;
+    private bool onSea;
+    //surfboard Transform properties
+    private float angle;
+    private bool jump = true;
+
+    public BoardCollision board;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector3(initialPlayerSpeed, 0.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //center position of the box, size of the box, roation around the z axis
+        //        onSea = Physics2D.OverlapBox(transform.position, transform.localScale, transform.rotation.z, whatIsSea);
+        onSea = board.getOnSea();
+        if (onSea)
+        {
+            jump = true;
+        }
+
+
+        //prevents jumping when paused button is pressed
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            //jump
+            if (Input.GetMouseButtonDown(0) && jump)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpVel);
+
+                if (!onSea)
+                {
+                    jump = false;
+                }
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            GameControl.instance.onEdibleTakeSound.Play();
+            other.gameObject.SetActive(false);
+            GameControl.instance.IncrementScore();
+        }
+        
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            //SceneManager.LoadScene(1);
+            GameControl.instance.Die();
+        }
+
+        if (other.gameObject.CompareTag("Balloon"))
+        {
+            GameControl.instance.balloonPopSound.Play();
+            rb.velocity = new Vector2(rb.velocity.x, balloonVel);
+            jump = true;
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Wave"))
+        {
+            rb.velocity = new Vector3(initialPlayerSpeed, rb.velocity.y, 0.0f);
+        }
+    }
+}
---
+++ Surfman/Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour {
+
+    public Transform playerTransform;
+
+    private SpriteRenderer sprite;
+
+	void Start () {
+
+        sprite = GetComponent<SpriteRenderer>();
+       
+        Debug.Log(sprite.bounds.extents.x);
+       Debug.Log(-sprite.bounds.extents.x);
+        Debug.Log(sprite.bounds.extents.y);
+        Debug.Log(-sprite.bounds.extents.y);
+        Debug.Log(playerTransform.position.x);
+
+    }
+
+    void Update()
+    {
+        if(playerTransform.position.x >= transform.position.x+sprite.bounds.extents.x*2)
+        {
+
+            Vector3 newTransform = new Vector3(sprite.bounds.extents.x * 4 + transform.position.x, 25,20);
+            transform.position = newTransform;
+           
+        }
+    }
+}
