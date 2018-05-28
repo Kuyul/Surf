3baa9b3617dbfbab8c64bc288ee0fe499c672b46
Kuyul <peyopeyo92@gmail.com>
2018-05-28 16:35:56 +1200

    Facebook and Firebase Implementation (1/2) complete
    Added Firebase Modules
    Added Facebook Login Modules
    Added a dummy leaderboard icon
    Added a dummy facebook Login Icon
    
    Require further updates on
    updating and reading from database

---
+++ Surfman/Assets/Scripts/FBScript.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FBScript : MonoBehaviour {
+
+    public GameObject DialogLoggedIn;
+    public GameObject DialogLoggedOut;
+    public GameObject DialogUsername;
+    public GameObject DialogProfilepic;
+    public GameObject LoginHandler;
+
+    private void Awake()
+    {
+        FacebookManager.Instance.InitFB();
+        DealWithFBMenus(FB.IsLoggedIn);
+    }
+
+    public void FBlogin()
+    {
+        List<string> permissions = new List<string>();
+        permissions.Add("public_profile");
+        permissions.Add("email");
+
+        FB.LogInWithReadPermissions(permissions, AuthCallBack); //we need to declare permissions
+    }
+
+    public void FBlogout()
+    {
+        FB.LogOut();
+
+        if (FB.IsLoggedIn)
+        {
+            Debug.Log("FB is logged in");
+        }
+        else
+        {
+            Debug.Log("FB is not logged in");
+        }
+        DealWithFBMenus(FB.IsLoggedIn);
+    }
+
+    void AuthCallBack(IResult result)
+    {
+        //First we want to deal with errors
+        if (result.Error != null)
+        {
+            Debug.Log(result.Error);
+        }
+        else
+        {
+            if (FB.IsLoggedIn)
+            {
+                FacebookManager.Instance.IsLoggedIn = true;
+                FacebookManager.Instance.GetProfile();
+                Debug.Log("FB is logged in");
+            }
+            else
+            {
+                Debug.Log("FB is not logged in");
+            }
+            DealWithFBMenus(FB.IsLoggedIn);
+        }
+    }
+
+    void DealWithFBMenus(bool isLoggedIn)
+    {
+        if (isLoggedIn)
+        {
+            //We don't want to reference the gameobjects that are specific to one scene in FacebookManager singleton
+            DialogLoggedIn.SetActive(true);
+            DialogLoggedOut.SetActive(false);
+
+            if (FacebookManager.Instance.ProfileName != null)
+            {
+                Text userName = DialogUsername.GetComponent<Text>();
+                userName.text = "Hi, " + FacebookManager.Instance.ProfileName + ", " + FacebookManager.Instance.Email;
+            }
+            else
+            {
+                StartCoroutine("WaitForProfileName");
+            }
+
+            if (FacebookManager.Instance.ProfilePic != null)
+            {
+                Image profilePic = DialogProfilepic.GetComponent<Image>();
+                profilePic.sprite = FacebookManager.Instance.ProfilePic;
+            }
+            else
+            {
+                StartCoroutine("WaitForProfilePic");
+            }
+        }
+        else
+        {
+            DialogLoggedIn.SetActive(false);
+            DialogLoggedOut.SetActive(true);
+        }
+    }
+
+    IEnumerator WaitForProfileName()
+    {
+        while(FacebookManager.Instance.ProfileName == null)
+        {
+            yield return null;
+        }
+
+        DealWithFBMenus(FacebookManager.Instance.IsLoggedIn);
+    }
+
+    IEnumerator WaitForProfilePic()
+    {
+        while (FacebookManager.Instance.ProfilePic == null)
+        {
+            yield return null;
+        }
+
+        DealWithFBMenus(FacebookManager.Instance.IsLoggedIn);
+    }
+}
---
+++ Surfman/Assets/Scripts/FacebookManager.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+using UnityEngine.UI;
+
+public class FacebookManager : MonoBehaviour {
+
+    private static FacebookManager _instance;
+
+    public static FacebookManager Instance
+    {
+        get
+        {
+            //create a new gameobject in the scene
+            if (_instance == null)
+            {
+                GameObject fbm = new GameObject("FBManager");
+                fbm.AddComponent<FacebookManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    public bool IsLoggedIn { get; set; }
+    public string ProfileName { get; set; }
+    public string Email { get; set; }
+    public Sprite ProfilePic { get; set; }
+    public Texture2D ProfileTexture{get; set;}
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        _instance = this;
+
+        IsLoggedIn = true;
+    }
+
+    public void InitFB()
+    {
+        if (!FB.IsInitialized)
+        {
+            FB.Init(SetInit, OnHideUnity);
+        }
+        else
+        {
+            IsLoggedIn = FB.IsLoggedIn;
+        }
+    }
+
+    void SetInit()
+    {
+        if (FB.IsLoggedIn)
+        {
+            Debug.Log("FB is logged in");
+        }
+        else
+        {
+            Debug.Log("FB is not logged in");
+        }
+        IsLoggedIn = FB.IsLoggedIn;
+    }
+
+    void OnHideUnity(bool isGameShown)
+    {
+        if (!isGameShown)
+        {
+            Time.timeScale = 0; //pause the game if the game isn't shown
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    public void GetProfile()
+    {
+        FB.API("/me?fields=first_name,email", HttpMethod.GET, DisplayUsername); //DisplayUsername callback
+        FB.API("/me/picture?type=square&height=128&width=128", HttpMethod.GET, DisplayProfilePic);
+    }
+
+    void DisplayUsername(IResult result)
+    {
+        if (result.Error == null)
+        {
+            ProfileName = "" + result.ResultDictionary["first_name"];
+            Email = "" + result.ResultDictionary["email"];
+            Debug.Log(ProfileName);
+        }
+        else
+        {
+            Debug.Log(result.Error);
+        }
+    }
+
+    //why graph result? :/
+    void DisplayProfilePic(IGraphResult result)
+    {
+        if (result.Texture != null)
+        {
+            ProfilePic = Sprite.Create(result.Texture, new Rect(0, 0, 128, 128), new Vector2());
+            ProfileTexture = result.Texture;
+        }
+    }
+
+    public string getAccessToken()
+    {
+        return AccessToken.CurrentAccessToken.TokenString;
+    }
+}
